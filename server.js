0bb4ece90f7a9bc9fe131246afe28e312d6b941d
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-17 14:43:23 -1000

    add middleware to check if there are buzzwords

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let score = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/buzzwords", (req, res) => {
   res.json({ buzzWords: buzzwords });
@@ -25,25 +25,32 @@ app.post("/buzzword", (req, res) => {
   }
 });
 
-app.put("/buzzword", (req, res) => {
+app.use((req, res, next) => {
   if (buzzwords.length === 0) {
     res.json({ success: false });
+    next();
   } else {
-    const word = buzzwords.filter(obj => {
-      console.log("obj: ", obj.buzzWord);
-      console.log("body word: ", req.body.buzzWord);
-      console.log("heard: ", req.body.heard);
-      if (obj.buzzWord === req.body.buzzWord && req.body.heard === "true") {
-        obj.heard = true;
-        score = Number(score) + Number(obj.points);
-        console.log(obj);
-        res.json({ success: true, newScore: score });
-      } else {
-        res.json({ success: false });
-      }
-    });
+    next();
   }
 });
+
+app.put("/buzzword", (req, res) => {
+  const word = buzzwords.filter(obj => {
+    console.log("obj: ", obj.buzzWord);
+    console.log("body word: ", req.body.buzzWord);
+    console.log("heard: ", req.body.heard);
+    if (obj.buzzWord === req.body.buzzWord && req.body.heard === "true") {
+      obj.heard = true;
+      score = Number(score) + Number(obj.points);
+      console.log(obj);
+      res.json({ success: true, newScore: score });
+    } else {
+      res.json({ success: false });
+    }
+  });
+});
+
+app.delete("/buzzword", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
