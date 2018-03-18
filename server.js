5d0cbd147d44660afafcdb22396695c889adc3ab
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-17 14:24:59 -1000

    handle post and put requests

--- server.js
+++ server.js
@@ -4,10 +4,46 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 8080;
+const buzzwords = [];
+let score = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.get("/buzzwords", (req, res) => {
+  res.json({ buzzWords: buzzwords });
+});
+
+app.post("/buzzword", (req, res) => {
+  console.log("body: ", req.body);
+  if (buzzwords.length < 4) {
+    buzzwords.push(req.body);
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+app.put("/buzzword", (req, res) => {
+  if (buzzwords.length === 0) {
+    res.json({ success: false });
+  } else {
+    const word = buzzwords.filter(obj => {
+      console.log("obj: ", obj.buzzWord);
+      console.log("body word: ", req.body.buzzWord);
+      console.log("heard: ", req.body.heard);
+      if (obj.buzzWord === req.body.buzzWord && req.body.heard === "true") {
+        obj.heard = true;
+        score = Number(score) + Number(obj.points);
+        console.log(obj);
+        res.json({ success: true, newScore: score });
+      } else {
+        res.json({ success: false });
+      }
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
